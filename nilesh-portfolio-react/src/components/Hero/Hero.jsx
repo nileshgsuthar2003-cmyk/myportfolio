2fd70783f8022385bab2f463ed30df28a2d76f2d
nileshgsuthar2003-cmyk <nilesh.g.suthar.2003@gmail.com>
2026-09-24 11:10:23 +0530

    signature re design

--- nilesh-portfolio-react/src/components/Hero/Hero.jsx
+++ nilesh-portfolio-react/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import scribbleSvg from '../../assets/scribble.svg';
+import Signature from './Signature';
 import ResumeModal from '../Navigation/ResumeModal';
 
 function Hero() {
@@ -37,23 +38,9 @@ function Hero() {
 
       {/* Hero Section */}
       <section className="hero-section">
-        {/* Cursive Signature SVG (Spells Nilesh) */}
+        {/* Authentic Handwritten Signature (Spells Nilesh) */}
         <div className="hero-signature-container">
-          <svg viewBox="0 0 450 160" fill="none" className="signature-svg">
-            <path 
-              d="M 25 100 C 20 60, 40 20, 50 20 C 55 20, 60 70, 70 120 C 75 140, 80 10, 90 20 C 100 30, 95 100, 110 80 C 115 70, 120 65, 125 70 C 130 75, 125 90, 135 90 C 140 70, 145 20, 155 20 C 160 20, 155 70, 160 90 C 165 80, 170 70, 175 70 C 180 70, 175 90, 185 90 C 195 90, 205 70, 200 65 C 195 60, 190 90, 205 90 C 215 70, 220 20, 228 20 C 232 20, 228 80, 232 90 C 236 100, 240 70, 248 70 C 255 70, 252 85, 260 90 C 270 95, 290 100, 310 100 C 330 100, 350 95, 370 90 C 390 85, 410 80, 420 85 C 430 90, 400 110, 350 120 C 270 135, 120 145, 50 140 C 30 138, 15 130, 20 120" 
-              stroke="#E48F5B" 
-              strokeWidth="7" 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-            />
-            <path 
-              d="M 123 45 A 2.5 2.5 0 1 1 128 45" 
-              stroke="#E48F5B" 
-              strokeWidth="6" 
-              strokeLinecap="round" 
-            />
-          </svg>
+          <Signature />
         </div>
 
         {/* Title / Header Group */}
